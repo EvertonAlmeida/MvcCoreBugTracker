c93cc71926d467f81da002ab146cdc6bd3e244f8
everton.oliveira <fP2d!p_#rqtM4N>
2021-02-12 07:04:41 -0300

    Add project files.

---
+++ src/MvcCoreBugTracker.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcCoreBugTracker.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
